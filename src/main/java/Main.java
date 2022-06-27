dabc99e90ec9755ba5bf00fcdc99393aff6b889f
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2022-06-27 15:18:08 +0200

    Boop!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args){
-        new ExampleWindow().run();
+import imgui.ImGui;
+import imgui.app.Application;
+import imgui.app.Configuration;
+
+public class Main extends Application {
+    @Override
+    protected void configure(Configuration config) {
+        config.setTitle("Dear ImGui is Awesome!");
+    }
+
+    @Override
+    public void process() {
+        ImGui.text("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        launch(new Main());
     }
 }
